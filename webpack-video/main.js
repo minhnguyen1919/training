8e83e006dd9388e08484bb14bed0a2bb2e4fb568
Minh Nguyen <minh.nguyen@asnet.com.vn>
2016-06-02 17:59:23 +0700

    webpack - without webpack.config.js file

---
+++ webpack-video/main.js
@@ -0,0 +1,3 @@
+require(['./src/bear/bear'], function (bear) {
+  document.body.appendChild(bear[0]);
+});
---
+++ webpack-video/src/bear/bear.js
@@ -0,0 +1,10 @@
+var $ = require('jquery');
+
+// css-loader convert bear.css into javascript code
+// style-loader apply javascript code to page
+
+var css = require('style!css!./bear.css');
+
+console.log(css);
+
+module.exports = $('<div/>').html('jquery action');
